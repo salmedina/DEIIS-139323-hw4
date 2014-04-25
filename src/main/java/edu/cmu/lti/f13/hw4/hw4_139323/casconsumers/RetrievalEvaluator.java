f1b7531f65fc32fc51dc1ed3ee8042495733ad5d
salmedina <zal@Rika.(none)>
2014-04-25 08:10:20 -0500

    Task 1 Completed, Missing another method

--- src/main/java/edu/cmu/lti/f13/hw4/hw4_139323/casconsumers/RetrievalEvaluator.java
+++ src/main/java/edu/cmu/lti/f13/hw4/hw4_139323/casconsumers/RetrievalEvaluator.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -25,6 +26,8 @@ import org.uimafit.util.FSCollectionFactory;
 import edu.cmu.lti.f13.hw4.hw4_139323.VectorSpaceRetrieval;
 import edu.cmu.lti.f13.hw4.hw4_139323.typesystems.Document;
 import edu.cmu.lti.f13.hw4.hw4_139323.typesystems.Token;
+import edu.cmu.lti.f13.hw4.hw4_139323.utils.Answer;
+import edu.cmu.lti.f13.hw4.hw4_139323.utils.Retrieval;
 
 
 public class RetrievalEvaluator extends CasConsumer_ImplBase {
@@ -35,20 +38,28 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 	/** query and text relevant values **/
 	public ArrayList<Integer> relList;
 	
+	/** document text **/
+	public ArrayList<String> textList;
+	
+	/** document vector represented by Maps**/
+	public ArrayList<Map<String, Integer>> documents;
+	
+	/** retrieval wrapper list**/
+	public ArrayList<Retrieval> retrievals;
+	
 	/** all the relevant words for analysis**/
 	public Set<String> globalWordSet;
 	
 	/** stopwords provided in a file**/
 	public Set<String> stopwordSet;
-	
-	/** document vector represented by Maps**/
-	public ArrayList<Map<String, Integer>> documents;
 		
 	public void initialize() throws ResourceInitializationException {
 
 		qIdList			= new ArrayList<Integer>();
 		relList			= new ArrayList<Integer>();
+		textList		= new ArrayList<String>();
 		documents 		= new ArrayList<Map<String, Integer>>();
+		retrievals		= new ArrayList<Retrieval>();
 		globalWordSet	= new HashSet<String>();
 		stopwordSet		= new HashSet<String>();
 		LoadStopwords();
@@ -80,13 +91,14 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 
 			qIdList.add(doc.getQueryID());
 			relList.add(doc.getRelevanceValue());
-
+			
 			//Do something useful here
+			textList.add(doc.getText());
 			Map<String, Integer> docVector = new HashMap<String, Integer>();
 			for (Token token : 
 				FSCollectionFactory.create(fsTokenList,Token.class)) {
-
 				String tokenText = token.getText();
+				//Only add word if it is not a stopword
 				if ( !stopwordSet.contains(tokenText)) {
 					globalWordSet.add(tokenText);
 					docVector.put(tokenText, token.getFrequency());
@@ -99,7 +111,7 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 	/**
 	 * TODO:
 	 * 		1. Compute Cosine Similarity (OK)
-	 * 		2. rank the retrieved sentences
+	 * 		2. rank the retrieved sentences 
 	 * 		3. Compute the MRR metric
 	 */
 	@Override
@@ -108,16 +120,11 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 
 		super.collectionProcessComplete(arg0);
 
-		// TODO :: compute the cosine similarity measure
+		parseRetrievals(retrievals);
 		
+		scoreRetrievals(retrievals);
 		
-		
-		// TODO :: compute the rank of retrieved sentences
-		
-		
-		
-		// TODO :: compute the metric:: mean reciprocal rank
-		double metric_mrr = compute_mrr();
+		double metric_mrr = compute_mrr(retrievals);
 		System.out.println(" (MRR) Mean Reciprocal Rank ::" + metric_mrr);
 	}
 
@@ -177,16 +184,79 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 	 * 
 	 * @return mrr
 	 */
-	private double compute_mrr() {
+	private double compute_mrr(List<Retrieval> retList) {
 		double metric_mrr=0.0;
 
-		// TODO :: compute Mean Reciprocal Rank (MRR) of the text collection
+		for(Retrieval retrieval : retList) {
+			metric_mrr += 1.0/(double)retrieval.computeRank();
+			printRetrieval(retrieval);
+		}
+		metric_mrr *= 1.0/retList.size();
 		
 		return metric_mrr;
 	}
 	
 	/**
+	 * Builds up a list of Retrieval objects from Document lists
+	 */
+	private void parseRetrievals(List<Retrieval> retList) {
+		Retrieval curRetrieval = null;
+		for(int i =0; i<qIdList.size(); ++i) {
+			int qid = qIdList.get(i);
+			int rel = relList.get(i);
+			String text = textList.get(i);
+			//Each retrieval starts with its query denoted
+			//with a relevance = 99
+			if( rel == 99) {
+				curRetrieval = new Retrieval(qid, text, documents.get(i) );
+				retList.add(curRetrieval);
+			}
+			else {
+				//Add answer to current retrieval
+				Answer curAnswer = new Answer();
+				curAnswer.QID		= qid;
+				curAnswer.Relevance	= rel;
+				curAnswer.Text		= text;
+				curAnswer.Vector	= documents.get(i);
+				curRetrieval.Answers.add(curAnswer);
+			}
+		}
+	}
+	
+	/**
 	 * 
+	 */
+	private void scoreRetrievals(List<Retrieval> retList) {
+		for(Retrieval curRetrieval:retrievals) {
+			for(Answer curAnswer: curRetrieval.Answers) {
+				curAnswer.Score = computeCosineSimilarity(curRetrieval.QueryVector, curAnswer.Vector);
+			}
+		}
+	}
+	
+	/**
+	 * Prints the retrieval information
+	 */
+	private void printRetrieval(Retrieval ret) {
+		String output = "";
+		output += "Query:\n";
+		output += "   QID:  "+ ret.QID +"\n";
+		output += "   Text: "+ ret.QueryText + "\n";
+		output += "   Rank: " + ret.Rank +"\n";
+		output += "Answers:\n";
+		for(Answer curAnswer: ret.Answers) {
+			output += "   Rel: "+curAnswer.Relevance;
+			output += String.format("  Score: %.5f", curAnswer.Score);
+			output += "  " + curAnswer.Text + "\n"; 
+		}
+		
+		
+		System.out.println(output);
+	}
+	
+	/**
+	 * Loads the stopwords provided in the original resources' file
+	 * Each line of the file contains a unique stopword
 	 */
 	private void LoadStopwords() {
 		try {
